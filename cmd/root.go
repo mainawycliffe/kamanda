71ab3d6eb05aad8f8642671765a212874debcb91
Maina Wycliffe <wmmaina7@gmail.com>
2019-12-03 12:03:12 +0300

    update description of the app

--- cmd/root.go
+++ cmd/root.go
@@ -12,20 +12,18 @@ import (
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "firebase-cli",
-	Short: "A companion tool for Firebase Tools",
-	Long: `This cli tool is mean as a companion tool for Firebase CLI Tool 
-  and should be used alongside it.
+	Short: "Kamanda is an extender Firebase Tools CLI",
+	Long: `Kamanda is a  Firebase CLI Tool extender and should be used alongside it. 
 
-  It provided addition functionality currently not available to via the Official 
-  Firebase Tool such as Managing Users, Databases etc from the CLI.
-  `,
+	Kamanda provides additional functionality currently not available to via the 
+	Firebase CLI Tool such as User Management, Cloud Firestore Management etc from the CLI.
+	
+	For instance, it allows you to easily create users with custom tokens, 
+	which is always a trick preposition.`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -36,21 +34,13 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.firebase-cli.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
 		// Find home directory.
@@ -60,14 +50,12 @@ func initConfig() {
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".firebase-cli" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".firebase-cli")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	viper.AutomaticEnv()
 
-	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
