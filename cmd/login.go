fbeb9f24c115f0b9ec7d2d7b9e2b9ae2d41087d3
Maina Wycliffe <wmmaina7@gmail.com>
2019-12-04 23:11:51 +0300

    feat: generate commands for login/logout

---
+++ cmd/login.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/mainawycliffe/kamanda/oauth"
+	"github.com/spf13/cobra"
+)
+
+// loginCmd represents the login command
+var loginCmd = &cobra.Command{
+	Use:   "login",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		oauth.StartLocalhostServer()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(loginCmd)
+}
