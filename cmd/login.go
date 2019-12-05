6f17229745f0b46dd726df89c25089df11085809
Maina Wycliffe <wmmaina7@gmail.com>
2019-12-05 20:30:19 +0300

    refactor: name variable appropriately

--- cmd/login.go
+++ cmd/login.go
@@ -16,8 +16,8 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		noLocalhost, _ := cmd.Flags().GetBool("no-localhost")
-		if noLocalhost {
+		noLocalhostFlag, _ := cmd.Flags().GetBool("no-localhost")
+		if noLocalhostFlag {
 			oauth.LoginWithoutLocalhost()
 		} else {
 			oauth.LoginWithLocalhost()
