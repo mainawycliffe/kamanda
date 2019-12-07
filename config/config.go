2f0576f6282be8e82669d513c1fe2d9378745733
Maina Wycliffe <wmmaina7@gmail.com>
2019-12-08 02:12:09 +0300

    feat: add method to save token

--- config/config.go
+++ config/config.go
@@ -1,6 +1,17 @@
 // Package config save configurations to file using github.com/spf13/viper
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/mitchellh/go-homedir"
+)
+
+const refreshTokenFilePath = ".kamanda/refresh_token.json"
+
 type RefreshToken struct {
 	ClientID     string `json:"client_id"`
 	ClientSecret string `json:"client_secret"`
@@ -8,6 +19,19 @@ type RefreshToken struct {
 	Type         string `json:"type"`
 }
 
-func SaveRefreshToken(token RefreshToken) {
-	panic("not implemented")
+// SaveRefreshToken saves refresh token to file
+func SaveRefreshToken(token RefreshToken) error {
+	home, err := homedir.Dir()
+	if err != nil {
+		return fmt.Errorf("Error reading home dir: %w", err)
+	}
+	jsonToken, err := json.MarshalIndent(token, "", " ")
+	if err != nil {
+		return fmt.Errorf("Error converting to json: %w", err)
+	}
+	err = ioutil.WriteFile(fmt.Sprintf("%s/%s", home, refreshTokenFilePath), jsonToken, os.ModeAppend)
+	if err != nil {
+		return fmt.Errorf("Error saving configs: %w", err)
+	}
+	return nil
 }
