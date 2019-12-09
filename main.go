b4265363efdd6959d070156f52777d3c35782d5e
Maina Wycliffe <wmmaina7@gmail.com>
2019-12-09 10:54:53 +0300

    chore: start using viper config isntead of os.env

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"log"
-
-	"github.com/joho/godotenv"
 	"github.com/mainawycliffe/kamanda/cmd"
 )
 
 func main() {
-	// this is temporary way of passing the credentials for a firebase project
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	cmd.Execute()
 }
