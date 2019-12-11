a35355e8a3b128ff15d1407b894766adbac31b06
Maina Wycliffe <wmmaina7@gmail.com>
2019-12-11 15:15:46 +0300

    fix: update command descriptions

--- cmd/logout.go
+++ cmd/logout.go
@@ -14,13 +14,7 @@ import (
 // logoutCmd represents the logout command
 var logoutCmd = &cobra.Command{
 	Use:   "logout",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Logout kamanda from Firebase",
 	Run: func(cmd *cobra.Command, args []string) {
 		email := viper.GetString(configs.FirebaseLoggedInUserEmailViperConfigKey)
 		if !viper.IsSet(configs.FirebaseRefreshTokenViperConfigKey) {
