c773f7358004747eb6ce731a1a0730818572970c
Maina Wycliffe <wmmaina7@gmail.com>
2019-12-13 05:19:54 +0300

    feat: add helper method to unmarshal json or yaml

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,15 @@
 package utils
 
+import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+
 	"github.com/logrusorgru/aurora"
+	"gopkg.in/yaml.v2"
+)
+
 // ProcessCustomClaimInput take in the input from cmd flags which a map of strings
 // and convert it to a map of interface
 func ProcessCustomClaimInput(input map[string]string) map[string]interface{} {
@@ -25,3 +33,25 @@ func StdOutSuccess(format string, a ...interface{}) {
 	fmt.Fprintf(os.Stdout, "%s\n", m)
 }
 
+// UnmashalFormatFile read and unmashal either a json/yaml file into a struct
+func UnmashalFormatFile(path string, extension string, v interface{}) error {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("Error reading %s: %w", path, err)
+	}
+	switch extension {
+	case "yaml":
+		err = yaml.Unmarshal(content, v)
+		if err != nil {
+			return fmt.Errorf("Error decoding yaml: %w", err)
+		}
+	case "json":
+		err = json.Unmarshal(content, v)
+		if err != nil {
+			return fmt.Errorf("Error decoding json: %w", err)
+		}
+	default:
+		return fmt.Errorf("Unsupported file type")
+	}
+	return nil
+}
