e16fa2cdcad1b62eca2f0985e853b44efbf8f738
Maina Wycliffe <wmmaina7@gmail.com>
2020-02-11 22:37:07 +0300

    refactor: update definition of firebase custom error

--- firebase/error.go
+++ firebase/error.go
@@ -2,37 +2,37 @@ package firebase
 
 import "firebase.google.com/go/auth"
 
-// Error custom error for firebase
-type Error struct {
-	OriginalError error
+// NewError return our error object with the original error inside
+func NewError(err error) FirebaseError {
+	return FirebaseError{
+		FirebaseError: err,
+	}
 }
 
-// NewError return our error object with the original error inside
-func NewError(err error) Error {
-	return Error{
-		OriginalError: err,
-	}
+// FirebaseError custom error for firebase
+type FirebaseError struct {
+	FirebaseError error
 }
 
 // Error parse the original error and return a coherent message
 // @todo: expand this to parse all Firebase errors and return a readable message
-func (err Error) Error() string {
-	if auth.IsEmailAlreadyExists(err.OriginalError) {
+func (err FirebaseError) Error() string {
+	if auth.IsEmailAlreadyExists(err.FirebaseError) {
 		return "Email already exists"
 	}
-	if auth.IsPhoneNumberAlreadyExists(err.OriginalError) {
+	if auth.IsPhoneNumberAlreadyExists(err.FirebaseError) {
 		return "Phone number already exists"
 	}
-	if auth.IsUIDAlreadyExists(err.OriginalError) {
+	if auth.IsUIDAlreadyExists(err.FirebaseError) {
 		return "UID already exists"
 	}
-	if auth.IsUserNotFound(err.OriginalError) {
+	if auth.IsUserNotFound(err.FirebaseError) {
 		return "User was not found"
 	}
-	if auth.IsUnknown(err.OriginalError) {
+	if auth.IsUnknown(err.FirebaseError) {
 		return "Unknown error"
 	}
-	return err.OriginalError.Error()
+	return err.FirebaseError.Error()
 }
 
 //IsUserNotFound check if an error is of type UserNotFound
