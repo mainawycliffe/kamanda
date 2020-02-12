7dcd3fde88b6b87a46d67d95324d1f4ccfe4ceee
Maina Wycliffe <wmmaina7@gmail.com>
2020-02-12 11:34:06 +0300

    test: add test for initializing firebase app
    currently checks if the fields of the struct \n are set correctly and app is not nil. might \n need to be more verbose inthe future

--- firebase/client_test.go
+++ firebase/client_test.go
@@ -1,6 +1,7 @@
 package firebase
 
 import (
+	"context"
 	"testing"
 )
 
@@ -43,6 +44,66 @@ func TestFirebase_setProjectID(t *testing.T) {
 			}
 			if f.projectId != tt.wantProjectId {
 				t.Errorf("Firebase.setProjectID() Project Id = %v, wanted %v", f.projectId, tt.wantProjectId)
+			}
+		})
+	}
+}
+
+func TestFirebase_initializeFirebaseApp(t *testing.T) {
+	type args struct {
+		ctx          context.Context
+		projectAlias string
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantErr        bool
+		wantAppToBeNil bool
+		wantProjectId  string
+	}{
+		{
+			"Test Firebase App Init",
+			args{
+				ctx:          context.Background(),
+				projectAlias: "default",
+			},
+			false,
+			false,
+			"kamanda-test-project",
+		},
+		{
+			"Test Firebase App Init (No Default Alias)",
+			args{
+				ctx:          context.Background(),
+				projectAlias: "",
+			},
+			false,
+			false,
+			"kamanda-test-project",
+		},
+		{
+			"Test Firebase App Init (No existent Default Alias)",
+			args{
+				ctx:          context.Background(),
+				projectAlias: "helloworld",
+			},
+			true,
+			true,
+			"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setViperConfigs(true)
+			f := &Firebase{}
+			if err := f.initializeFirebaseApp(tt.args.ctx, tt.args.projectAlias); (err != nil) != tt.wantErr {
+				t.Errorf("Firebase.initializeFirebaseApp() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if (f.App == nil) != tt.wantAppToBeNil {
+				t.Errorf("Firebase.initializeFirebaseApp() Firebase.App is nil = %v, want Firebase.App to be nil = %v", (f.App == nil), tt.wantAppToBeNil)
+			}
+			if f.projectId != tt.wantProjectId {
+				t.Errorf("Firebase.initializeFirebaseApp() Firebase.projectId = %v, want = %v", f.projectId, tt.wantProjectId)
 			}
 		})
 	}
