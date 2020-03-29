66e9b066e0cb413a5f0400e35c455ace4e8c1549
Maina Wycliffe <wmmaina7@gmail.com>
2020-03-29 23:50:06 +0300

    Improve UI for Viewing Users

--- cmd/listUsers.go
+++ cmd/listUsers.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/gdamore/tcell"
 	"github.com/mainawycliffe/kamanda/firebase/auth"
@@ -34,21 +35,27 @@ var listUsersCmd = &cobra.Command{
 		// @todo: set the width of the table to 100%
 		app := tview.NewApplication()
 		table := tview.NewTable()
-		tableHeaderColumnNames := []string{"#", "UID", "Email", "Name", "Phone Number"}
+		tableHeaderColumnNames := []string{"#", "UID", "Email", "Email Verified", "Name", "Phone Number", "Provider"}
 		for index, value := range tableHeaderColumnNames {
-			tableCell := tview.NewTableCell(value).SetTextColor(tcell.ColorWhite).SetBackgroundColor(tcell.ColorBlue)
+			tableCell := tview.NewTableCell(value).
+				SetTextColor(tcell.ColorBlue).
+				SetSelectable(false).
+				SetStyle(tcell.StyleDefault).
+				SetAlign(tview.AlignLeft)
 			table.SetCell(0, index, tableCell)
 		}
-		for i, v := range getUsers.Users {
-			isEmailVerified := ""
-			if !v.EmailVerified {
-				isEmailVerified = "(Not Verified)"
+		for index, user := range getUsers.Users {
+			isEmailVerified := "Yes"
+			if !user.EmailVerified {
+				isEmailVerified = "No"
 			}
-			table.SetCell(i+1, 0, tview.NewTableCell(fmt.Sprintf("%d", i+1))).SetSelectable(false, false)
-			table.SetCell(i+1, 1, tview.NewTableCell(v.UID)).SetSelectable(true, false)
-			table.SetCell(i+1, 2, tview.NewTableCell(fmt.Sprintf("%s (%s)", v.Email, isEmailVerified))).SetSelectable(true, false)
-			table.SetCell(i+1, 3, tview.NewTableCell(v.DisplayName)).SetSelectable(true, false)
-			table.SetCell(i+1, 4, tview.NewTableCell(v.PhoneNumber)).SetSelectable(true, false)
+			table.SetCell(index+1, 0, tview.NewTableCell(fmt.Sprintf("%d", index+1))).SetSelectable(false, false)
+			table.SetCell(index+1, 1, tview.NewTableCell(user.UID).SetExpansion(3)).SetSelectable(true, false)
+			table.SetCell(index+1, 2, tview.NewTableCell(user.Email).SetExpansion(2)).SetSelectable(true, false)
+			table.SetCell(index+1, 3, tview.NewTableCell(isEmailVerified).SetExpansion(1)).SetSelectable(true, false)
+			table.SetCell(index+1, 4, tview.NewTableCell(user.DisplayName).SetExpansion(3)).SetSelectable(true, false)
+			table.SetCell(index+1, 5, tview.NewTableCell(user.PhoneNumber).SetExpansion(1)).SetSelectable(true, false)
+			table.SetCell(index+1, 6, tview.NewTableCell(strings.ToUpper(user.ProviderID)).SetExpansion(1)).SetSelectable(true, false)
 		}
 		table.Select(1, 0).SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
 			// exit when the following keys ae pressed
@@ -58,14 +65,16 @@ var listUsersCmd = &cobra.Command{
 			if key == tcell.KeyEnter {
 				table.SetSelectable(true, true)
 			}
-		}).SetSelectedFunc(func(row int, column int) {
-			uid := table.GetCell(row, 1).Text
-			fmt.Printf("%s\n", uid)
-			// @todo: fetch from firebase the user details
-
-		})
+		}).
+			// @todo: show more info about the user when a row is clicked
+			SetSelectedFunc(func(row int, column int) {
+				// @todo: fetch from firebase the user details
+				// uid := table.GetCell(row, 1).Text
+			})
+		tableContainer := tview.NewFlex().SetDirection(tview.FlexRow).AddItem(table, 0, 1, true)
+		appContainer := tview.NewFlex().AddItem(tableContainer, 0, 1, true)
 		// @todo add pagination to get more request
-		if err := app.SetRoot(table, true).Run(); err != nil {
+		if err := app.SetRoot(appContainer, true).Run(); err != nil {
 			utils.StdOutError(os.Stderr, "Error! %s", err.Error())
 			os.Exit(1)
 		}
