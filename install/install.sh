5f340a30bea2f5922ddf3e802818edd37201ffbf
Maina Wycliffe <wmmaina7@gmail.com>
2020-05-31 03:00:52 +0300

    Add Bash Script to Install kamanda on unix systems
    Issue #15

---
+++ install/install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -xe
+
+# determine os
+case $(uname -s) in
+    Darwin) platform='darwin';;
+    *) platform='linux';;
+esac
+
+# determine architecture
+case $(uname -m) in
+    "x86_64") arch="amd64";; 
+    *) arch="386";;
+esac
+
+
+if [ $# -eq 0 ]; then
+    # download the latest executables for kamanda 
+	kamanda_asset_path=$(
+		command curl -sSf https://github.com/mainawycliffe/kamanda/releases |
+			command grep -o "/mainawycliffe/kamanda/releases/download/.*/kamanda_.*_${platform}_${arch}.tar.gz" |
+			command head -n 1
+	)
+    if [ ! "$kamanda_asset_path" ]; then exit 1; fi
+    downloadUrl="https://github.com/${kamanda_asset_path}"
+else
+    # download the specific requested version for kamanda
+    downloadUrl="https://github.com/mainawycliffe/kamanda/releases/download/v${1}/kamanda_${1}_${platform}_${arch}.tar.gz"
+fi
+
+binDir="/usr/local/bin"
+filename="kamanda"
+
+curl --fail --location --progress-bar "${downloadUrl}" --output "./${filename}.tar.gz"
+
+# extract executables
+tar xvzf kamanda.tar.gz
+
+# add to use executable dir, no permissions required
+mv ${filename} ${binDir}
+
+# delete the extra files
+echo "Clean up the dowloads after installation"
+rm "LICENSE" "README.md" "./${filename}.tar.gz"
+
+echo "Kamanda was installed successfully. You can find documentation here https://kamanda.dev"
